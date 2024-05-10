cdcb618de24b06cc730854c19cf78d70a45c5cf4
John Iweh <iwehjohn@gmail.com>
2024-05-10 06:29:52 +0100

    modified CSS color theme

--- icallon-fe/app/page.js
+++ icallon-fe/app/page.js
@@ -1,12 +1,16 @@
-import Image from "next/image";
+// import Image from "next/image";
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <>
-      <div className="grid bg-gradient-landing w-screen h-screen place-content-center p-4 gap-16">
+      <div className="grid w-screen h-screen place-content-center p-4 gap-16">
         <img className="" src="/app_logo.png" alt="App Logo" />
-        <div>
-          <button>startgame</button>
+        <div className="flex justify-center">
+          <Button asChild size="lg" >
+            <Link href="#">Start Game</Link>
+          </Button>
         </div>
       </div>
     </>
