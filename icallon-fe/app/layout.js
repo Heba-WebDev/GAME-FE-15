acc52d0990f6c2d87bee073e21d1e39af5545c6d
John Iweh <iwehjohn@gmail.com>
2024-05-11 02:36:43 +0100

    Added font and buttons

--- icallon-fe/app/layout.js
+++ icallon-fe/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Fuzzy_Bubbles } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fuzzy_bubbles = Fuzzy_Bubbles({
+  subsets: ["latin"],
+  display: "swap",
+  weight: '400',
+  variable: "--fuzzy_bubbles",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fuzzy_bubbles.className}>{children}</body>
     </html>
   );
 }
--- icallon-fe/app/page.js
+++ icallon-fe/app/page.js
@@ -1,15 +1,36 @@
+'use client'
+
 // import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [showGameOption, setShowGameOption] = useState(false);
   return (
     <div className="grid bg-gradient-to-r from-[#1EA8B1] from-5% via-transparent via-50% to-[#1EA8B1] to-95% w-screen h-screen place-content-center p-4 gap-16">
       <img className="" src="/app_logo.png" alt="App Logo" />
       <div className="flex justify-center">
-        <Button asChild size="lg">
-          <Link href="/start">Start Game</Link>
-        </Button>
+        {!showGameOption && (
+          <Button className="bg-gradient-to-b from-accent via-white/55 to-accent" size="lg" onClick={() => setShowGameOption(true)}>
+            Start Game
+          </Button>
+        )}
+        {showGameOption && (
+          <div className="grid gap-5 pl-24 pr-24 w-full">
+            <Button asChild variant="outline" size="lg">
+              <Link href="/start">Solo Game</Link>
+            </Button>
+
+            <Button asChild variant="outline" size="lg">
+              <Link href="/start">Create/Join a group</Link>
+            </Button>
+
+            <Button asChild variant="outline"  size="lg">
+              <Link href="/start">Tornament Mode</Link>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
