d0442305a9e87b81809b9b8fed35ffeadead1296
John Keno Iweh <iwehjohn@gmail.com>
2024-05-12 22:35:28 +0100

    hot fix deployment

--- icallon-fe/lib/authOptions.js
+++ icallon-fe/lib/authOptions.js
@@ -3,21 +3,22 @@ import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import Credentials from "next-auth/providers/credentials";
 
-
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-        // Credentials({
-        //     credentials:{
-        //         email: {},
-        //         password: {},
-        //     },
-        //     authorize: async(credentials, req) => {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
 
-        //     }
-        // })
-    ]
-}
+    // Credentials({
+    //     credentials:{
+    //         email: {},
+    //         password: {},
+    //     },
+    //     authorize: async(credentials, req) => {
+
+    //     }
+    // })
+  ],
+  secret: process.env.AUTH_SECRET
+};
