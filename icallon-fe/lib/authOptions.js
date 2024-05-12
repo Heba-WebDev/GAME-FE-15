30b25927f38651274f0e948024ae40211b37f9a3
John Keno Iweh <iwehjohn@gmail.com>
2024-05-12 21:53:28 +0100

    fix env for deployment

--- icallon-fe/lib/authOptions.js
+++ icallon-fe/lib/authOptions.js
@@ -1,17 +1,23 @@
 // import {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import Credentials from "next-auth/providers/credentials";
 
 
 export const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
         }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        })
+        // Credentials({
+        //     credentials:{
+        //         email: {},
+        //         password: {},
+        //     },
+        //     authorize: async(credentials, req) => {
+
+        //     }
+        // })
     ]
 }
