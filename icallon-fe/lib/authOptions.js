edc36d8d76a2c2ff8af6aaf02433e242966d2f69
John Keno Iweh <iwehjohn@gmail.com>
2024-05-13 23:49:35 +0100

    temp setup auth

--- icallon-fe/lib/authOptions.js
+++ icallon-fe/lib/authOptions.js
@@ -1,6 +1,5 @@
 // import {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
@@ -10,16 +9,54 @@ export const authOptions = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
 
-    // Credentials({
-    //     credentials:{
-    //         email: {},
-    //         password: {},
-    //     },
-    //     authorize: async(credentials, req) => {
+    Credentials({
+      id: "credentials",
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Email",
+      credentials: {
+        email: {
+          label: "email",
+          type: "email",
+          placeholder: "john@example.com",
+        },
+        password: { label: "Password", type: "password", placeholder: "password" },
+      },
+      authorize: async (credentials, req) => {
+        const payload = {
+          email: credentials.email,
+          password: credentials.password,
+        };
 
-    //     }
-    // })
+        const res = await fetch(
+          "https://game-be-15.onrender.com/api/v1/players/login",
+          {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const user = await res.json();
+        if (!res.ok) {
+
+          throw new Error(user.message);
+        }
+        // checks if response is ok and data is retrieved
+        if (res.ok && user) {
+          return user;
+        }
+         // Return null if user data could not be retrieved
+         return null
+      },
+    }),
   ],
   //added secret
-  secret: process.env.AUTH_SECRET
+  secret: process.env.AUTH_SECRET,
+  theme: {
+    colorScheme: 'light', // "auto" | "dark" | "light
+    backgroundColor:'red',
+    brandColor: '', // Hex color code #33FF5D
+    logo: '/app_logo.png', // Absolute URL to image
+  },
 };
