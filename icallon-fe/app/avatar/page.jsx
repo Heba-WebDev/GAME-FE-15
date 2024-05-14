2252a37b26a34d986bd788870ab1abf26ac2558c
lawanaji <lawanmustaphaaji2015@gmail.com>
2024-05-14 19:38:20 +0100

    css changes

--- icallon-fe/app/avatar/page.jsx
+++ icallon-fe/app/avatar/page.jsx
@@ -13,7 +13,7 @@ export default function Avatar() {
   return (
     <div className="w-full p-5">
     <Cross2Icon width={50} height={50}/>
-    <div className=" md:flex h-[80vh] justify-around items-center w-full">
+    <div className=" md:flex md:h-[80vh] justify-around items-center w-full">
      <div className='w-full md:w-[50%]'>
      {previewAvatar ? (
         <div className="border-2 bg-gradient-to-r from-[#1EA8B1] from-5% via-transparent via-50% to-[#1EA8B1] w-full md:w-[50%] md:h-[60vh] flex justify-center items-center object-contain">
@@ -26,9 +26,9 @@ export default function Avatar() {
       )}
      </div>
 
-      <div className="">
+      <div className=" py-10 space-y-5">
         <h1>Josiah, please select a base Avatar to be onboarded</h1>
-        <div className="grid grid-rows-3 grid-flow-col md:grid-rows-3 md:grid-flow-col md:gap-4 space-x-2">
+        <div className="grid grid-rows-5 gap-2 grid-flow-col md:grid-rows-3 md:grid-flow-col md:gap-4 space-x-2">
           {/* Add your avatar options here */}
           <div className='bg-gradient-to-r from-[#1EA8B1] from-5% via-transparent via-50% to-[#1EA8B1] w-[150px] px-5 hover:border-[#FC9A02] border'>
           <img src="/maleavatar1.svg" alt="" onClick={handlePreview} />
@@ -70,8 +70,8 @@ export default function Avatar() {
       
     </div>
        
-   <div className='flex justify-between mb-10 px-[10%]'>
-    <button className='bg-[#FC9A02] shadow-md p-4 font-extralight'>Use Avatar </button>
+   <div className='flex justify-between md:mb-15 px-[10%]'>
+    <button className='bg-[#FC9A02] shadow-md p-3 font-extralight'>Use Avatar </button>
     <SpeakerLoudIcon width={50} height={50}/>
    </div>
   </div>
