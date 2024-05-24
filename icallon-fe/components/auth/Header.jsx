2e874844c647c906de860986f6bbcadfbcc87b84
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-25 00:43:49 +0300

    Added a link to wrap the logo so users can go back to the home page if the logo is clicked

--- icallon-fe/components/auth/Header.jsx
+++ icallon-fe/components/auth/Header.jsx
@@ -1,8 +1,9 @@
 'use client'
+import Link from "next/link";
 export function Header({ label }) {
   return (
     <div className="flex w-full flex-col gap-y-4 items-center justify-center">
-      <h1 className="text-3xl font-semibold"><img src="/app_logo.png" alt="" width={200} height={200}/></h1>
+      <Link href="/" className="text-3xl font-semibold"><img src="/app_logo.png" alt="" width={200} height={200}/></Link>
       <p className="text-lg"> {label}</p>
     </div>
   );
