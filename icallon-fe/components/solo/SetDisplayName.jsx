11340ad4cfb590d8c902713dffa31db144892b05
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-26 00:17:49 +0300

    Added needed css changes to support smaller screens

--- icallon-fe/components/solo/SetDisplayName.jsx
+++ icallon-fe/components/solo/SetDisplayName.jsx
@@ -14,8 +14,18 @@ export default function SetDisplayName({
 }) {
 
   return (
-    <main className=" flex justify-between items-center h-screen w-[80%]">
-      <div className="bg-[#404040] h-[80%] w-2/12 p-4 flex flex-col justify-between">
+    <main className=" w-full h-screen md:w-[80%]">
+      <nav className=" p-4 flex items-center md:hidden">
+      <Button
+            variant="ghost"
+            size="small"
+            onClick={() => goBack(currentPage)}
+          >
+            <span className=" text-2xl pr-2">&lt;</span> back
+          </Button>
+      </nav>
+      <section className="flex flex-col-reverse md:flex-row md:gap-2 justify-between items-center h-screen w-full px-2 ">
+      <div className="bg-[#404040] h-[80%] md:w-[23%] lg:w-2/12 p-4 hidden md:flex flex-col justify-between">
         <div className="flex flex-col items-center gap-4">
           <Image src={logo2} alt="" className="w-36 mx-auto" />
           <Button variant="outline" size="lg">
@@ -44,19 +54,31 @@ export default function SetDisplayName({
           </Button>
         </div>
       </div>
-      <div className="flex flex-col justify-between w-2/4 h-[80%] ">
-        <h1 className="text-4xl text-input w-full">
+      <div className="flex w-full md:w-[515px] flex-col justify-between h-[80%]">
+        <h1 className="text-4xl text-input w-full hidden md:block">
           Enter your preferred username
         </h1>
-        <Image src={sheet} alt="" className="h-4/5 w-full object-cover" />
+        <Image src={sheet} alt="" className=" w-full object-cover" />
+        <Button
+          size="small"
+          className="w-36 font-bold block md:hidden py-2 mb-2 self-end"
+          onClick={() => {
+            nextPage(currentPage);
+          }}
+        >
+          Continue
+        </Button>
       </div>
-      <div className="flex flex-col items-center justify-between h-[80%] w-2/12  ">
-        <div className=" pt-[50%] space-y-4">
+      <div className="flex flex-col w-full items-center justify-between h-[80%] md:w-2/12  ">
+        <div className=" pt-[50%] space-y-3 md:space-y-4 mb-5 md:mb-0">
           {data.user.avatar ? (
-            <Image src={data.user.avatar?.src} alt={data.user.avatar?.alt} />
+            <Image src={data.user.avatar?.src} alt={data.user.avatar?.alt} className="mx-auto" />
           ) : (
-            <Image src={selectedAvatar?.src} alt={selectedAvatar?.alt} />
+            <Image src={selectedAvatar?.src} alt={selectedAvatar?.alt} className="mx-auto"/>
           )}
+          <h1 className="text-input w-full block md:hidden">
+          Enter your preferred username
+        </h1>
           <input
             type="text"
             placeholder="Display Name"
@@ -66,7 +88,7 @@ export default function SetDisplayName({
         </div>
         <Button
           size="lg"
-          className="w-full font-bold"
+          className="w-full font-bold hidden md:block"
           onClick={() => {
             nextPage(currentPage);
           }}
@@ -74,6 +96,7 @@ export default function SetDisplayName({
           Continue
         </Button>
       </div>
+      </section>
     </main>
   );
 }
